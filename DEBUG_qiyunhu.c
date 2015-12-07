5c294e7fe360461064c79020f79d84cb14bf33da
qiyunhu <qiyunhu@bbktel.com>
2015-12-07 17:52:45 +0800

    routine push

--- DEBUG_qiyunhu.c
+++ DEBUG_qiyunhu.c
@@ -1,29 +1,12 @@
 
 
 #define DEBUG_qiyunhu 1
+#define __SFILE__  (strrchr(__FILE__,'/')?strrchr(__FILE__,'/')+1:__FILE__)
 
 #ifdef DEBUG_qiyunhu
-#ifndef NULL
-#define NULL 0
-#endif
-char *strrchr_(register const  char *s, int c)
-{
-    register const char *p; 
-    p = NULL;
-    do {
-        if (*s == (char) c) {
-            p = s;
-        }
-    } while (*s++); 
-    return (char *) p;          /* silence the warning */
-}
-#define __filename__ (strrchr_(__FILE__, '/') ? strrchr_(__FILE__, '/') + 1 : __FILE__)
-#endif 
-
-#ifdef DEBUG_qiyunhu
-ALOGE( "[+] FILE %s : %d FUNCTION %s called |||||||||||||||||" , __LINE__ , __filename__ , __func__ ) ;
+ALOGE( "[+] FILE %s : %d FUNCTION %s called <=================================\n" , __SFILE__ , __LINE__ , __func__ ) ;
 #endif
 
 #ifdef DEBUG_qiyunhu
-pr_err( "[+] FILE %s : %d FUNCTION %s called ________________" , __filename__ , __LINE__ , __func__ ) ;
+pr_err( "[+] FILE %s : %d FUNCTION %s called <________________\n" , __FILE__ , __LINE__ , __func__ ) ;
 #endif
